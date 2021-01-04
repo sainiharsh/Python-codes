a429fa949204fccf3e0a373f379f654efe217c20
Harsh <sainiharsh22@gmail.com>
2021-01-04 22:12:18 +0530

    modules and package

---
+++ modules/calculator.py
@@ -0,0 +1,24 @@
+CONSTANT = "HARSH SAINI"
+ 
+class A:
+    pass
+    
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a//b
+
+a = 1
+b = 1 
+c = 1
+d = 1
+e = 1
+f = 1
---
+++ modules/main.py
@@ -0,0 +1,116 @@
+import math
+
+print(math.a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print("Hello world ! ")
+
+# def func():
+#     print("Hello")
+
+
+# func()
+'''import calculator  # first method to use modules
+
+
+print(type(calculator))
+
+print(calculator.add(5,10))
+print(calculator.sub(50,10))
+print(calculator.div(500,10))
+print(calculator.mul(50,10))
+a = calculator.A()
+print(type(a))  '''
+
+'''
+from calculator import add   # 2nd method to use modules 
+ # only add function will be called to main
+                            
+print(add(1,2))
+print(calculator)
+'''
+
+# from calculator import (
+#     add,
+#     sub,
+#     a,
+#     b,
+#     c,
+#     d,
+#     e,
+# )
+ 
+
+# #print(globals())
+# print(add(1,2))
+# print(sub(10,5))
+# print(a)
+
+#ALISIES
+#import calculator as lib
+
+
+# from calculator import(
+#     add as addition,
+#     sub as difference
+# )
+
+
+# def calculator(a,b):
+#     print(a,b)
+
+
+# # print(lib.add(1,2))
+# # print(lib.sub(1,2))
+# print(addition(1,2))
+# print(difference(10,5))
+# calculator(1,2)
+
+
+
+
+
+
+
+
+
+
+
+
